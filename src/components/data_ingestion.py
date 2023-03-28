b468a8833cb24dcc46f61eb0cf3cb767e1100547
AshfaqurProtick <ashfaqur.protick@gmail.com>
2023-03-28 14:57:38 +0600

    First Change

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,11 +7,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
+
+# a class that is designed to only hold data values
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -28,6 +31,7 @@ class DataIngestion:
             df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
+# os.path.dirname() method in Python is used to get the directory name from the specified path.
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
@@ -39,12 +43,11 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Inmgestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
         except Exception as e:
             raise CustomException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -2,6 +2,9 @@ import logging
 import os
 from datetime import datetime
 
+# os.path.join() combines path names into one complete path
+# makedirs creates all directories that are part of path and do not yet exist
+
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
